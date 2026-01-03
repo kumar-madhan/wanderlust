dd873357f4761802f25e582b4cb80a78bc8b683b
kumar-madhan <madhankumar150598@gmail.com>
2026-01-03 15:48:43 +0000

    jenkins build fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,9 @@
-import { defineConfig, UserConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import { configDefaults } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [react()] as UserConfig['plugins'],
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    coverage: {
-      provider: 'v8',
-    },
-    reporters: ['verbose'],
-    exclude: [
-      ...configDefaults.exclude,
-      './src/__tests__/integration-test/home.test.tsx',
-      './src/__tests__/App.test.tsx',
-    ],
-
-    setupFiles: './test-setup.ts',
-  },
-
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
